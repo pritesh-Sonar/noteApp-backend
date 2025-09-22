66b178058c92ce725dcae043bd1ebeb652376b04
pritesh sonar <154061984+pritesh-Sonar@users.noreply.github.com>
2025-09-22 23:38:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ const app = express();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
-require('dotenv').config();
 
 
 app.use(express.json())
